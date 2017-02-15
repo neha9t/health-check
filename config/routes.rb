81094309c778b459e69a488d68249f4d95b01b25
neha <neha@pstakecare.com>
2017-02-15 13:40:33 +0530

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+require 'sidekiq/web'
+mount Sidekiq::Web => '/sidekiq'
+
 post '/details/new', to: 'check#new'
-get '/details', to: 'check#index'
+get '/', to: 'check#index'
+get '/show', to: 'check#show'
 get '/details/:id', to: 'check#check'
 put '/details/:id', to: 'check#update'
-delete '/:id', to: 'check#delete' 
+delete '/:id', to: 'check#delete'
+get '/:id/delete' , to: 'check#deletion_details'
 end
