1e6e4cc62e13918b0ae11e4099e0c50bb10d82cc
neha <neha@pstakecare.com>
2017-02-15 13:30:54 +0530

    Add check controller

--- Gemfile
+++ Gemfile
@@ -35,6 +35,12 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'rest-client'
+
+gem "sidekiq"
+gem 'slim'
+gem 'sendgrid-ruby'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/check_controller.rb
+++ app/controllers/check_controller.rb
@@ -1,41 +1,44 @@
+require_relative '../jobs/sidekiq'
+
 class CheckController < ApplicationController
 
 # post '/details/new' do
+  def show
+  	render "new"
+  end
+
   def new
-    params.to_s
-    "Hello, world, I am the new change!"
-    @details = FormDetails.new(params)
-    @details.save
+    @details = Check.create(check_params)
     # binding.pry
     if params[:enabled] == "true"
-      HealthCheck.perform_async(@details.id)
+      # HealthCheck.perform_async(@details.id)
     end
     @details.to_json
-    redirect to("/details")
+    redirect_to("/")
   end
 
   # get '/details' do
   def index
     # protected!
     # if authorized?
-      @details = FormDetails.all(:order => :created_at.desc)
-      redirect to('/show') if @details.empty?
+      @details = Check.order(created_at: :desc)
+      # redirect_to('/show') and return if @details.empty?
       @details.to_json
-      erb :view_all
+      render "view_all"
     # end
   end
 
   # get '/details/:id' do
   def check  
-    @details = FormDetails.get(params[:id])
+    @details = Check.find(params[:id])
     @title = "Edit note ##{params[:id]}"
     @details.to_json
-    erb :edit
+    render "edit"
   end
 
   # put '/details/:id' do
   def update
-    @details = FormDetails.get(params[:id])
+    @details = Check.find(params[:id])
     if params[:enabled] != @details.enabled
       if params[:enabled] == true
         HealthCheck.perform_async(@details.id)
@@ -43,26 +46,30 @@ class CheckController < ApplicationController
         
       end
     end
-    @details.method_name = params[:method_name]
     @details.interval = params[:interval]
     @details.url = params[:url]
     @details.enabled = params[:enabled]
     @details.save
     @details.to_json
-    redirect to("/details")
+    redirect_to("/")
   end
 
   # get '/:id/delete' do
-  #   @details = FormDetails.get(params[:id])
-  #   @title = "Edit note ##{params[:id]}"
-  #   erb :delete
-  # end
+  def deletion_details
+    @details = Check.find(params[:id])
+    @title = "Edit note ##{params[:id]}"
+    render "delete"
+  end
 
   # delete '/:id' do
   def delete  
-    @details = FormDetails.get(params[:id])
+    @details = Check.find(params[:id])
     @details.destroy
-    redirect to("/details")
+    redirect_to("/")
   end
+
+  def check_params
+      params.permit(:url, :enabled, :interval)
+    end
 
 end
