69e41640968d87da40959c6f5410d45b1b04ab79
neha <neha@pstakecare.com>
2017-02-20 14:43:37 +0530

    remove binding pry & add after_commit

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,10 +1,9 @@
 class Check < ApplicationRecord
   puts "I am inside Model"
-  after_commit :run_sidekiq, :on => :create
+  after_commit :run_sidekiq, on: :create
 
   private
     def run_sidekiq
-      binding.pry
  #     if self.enabled == "true"
         puts "I am inside after_commit trigger run_sidekiq"
         HealthCheckWorker.perform_async(self.id)
