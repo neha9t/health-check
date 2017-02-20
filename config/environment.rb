9abd612de90c899d729858394648b4b229fd050e
neha <neha@pstakecare.com>
2017-02-20 14:42:06 +0530

    Add Settings of action mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require_relative 'application'
 
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => 'neha9t',
+#   :password => 'Ginger@123',
+#   :domain => 'gmail.com',
+#   :address => 'smtp.gmail.com',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
+
 # Initialize the Rails application.
 Rails.application.initialize!
