b0aa5a9bb8f146aaf68fbd060d891acc17afb9f7
Neha Tandon <nehatandon1@gmail.com>
2017-09-18 19:09:40 +0530

    Add default values function for keeping enabled false value

--- app/models/check.rb
+++ app/models/check.rb
@@ -2,15 +2,28 @@ class Check < ApplicationRecord
 
   after_commit :run_sidekiq, on: :create
   before_destroy :destroy_sidekiq_jobs
+  before_save :default_values
+
+ 
 
   private
-    def run_sidekiq
- #     if self.enabled == "true"
-        puts "I am inside after_commit trigger run_sidekiq"
-        HealthCheckWorker.perform_async(self.id)
-  #    end
+
+    def default_values
+      self.enabled||= false
+      if self.enabled == false
+        self.last_run ||= "Awaiting: #{Time.now}"
+      end
     end
+
+    def run_sidekiq
+     if self.enabled == true
+        puts "Model method - triggered sidekiq"
+        HealthCheckWorker.perform_async(self.id, self.url)
+     end
+    end
+    
     def destroy_sidekiq_jobs
+    binding.pry
       scheduled = Sidekiq::ScheduledSet.new
       scheduled.each do |job|
         if job.klass == 'HealthCheckWorker' && job.args.first == id
